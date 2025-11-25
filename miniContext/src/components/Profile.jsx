fe4770b5cc124f42ac35c08603204813b2538503
saurabh <jaiswalsaurabh888@gmail.com>
2025-11-25 22:16:33 +0530

    sa

--- miniContext/src/components/Profile.jsx
+++ miniContext/src/components/Profile.jsx
@@ -5,7 +5,7 @@ function Profile() {
   const { user } = useContext(UserContext);
   if (!user) return <div>Please Login</div>;
 
-  return <div>Welcome{user.userName}</div>;
+  return <div>Welcome {user.userName}</div>;
 }
 
 export default Profile;
